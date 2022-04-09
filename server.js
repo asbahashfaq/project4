4fa626f79699705bf7598fc82be7e1e65c57a45a
asbahashfaq <asbah.ashfaq@live.com>
2022-04-10 00:31:17 +1000

    deployment fixes

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ server.listen(port,
 app.use(logger)  
 app.use(sessions)
   
-app.use(express.static(path.resolve(__dirname, "./frontend/build")));
+
 
 //routes 
 app.use('/parents', parentsController)
@@ -85,39 +85,11 @@ app.use('/children', childrenController)
 app.use('/users', usersController)
 app.use('/contacts', contactsController)
 
-
-
+if (process.env.NODE_ENV === 'production') {
+    const path = require('path')
+    app.use(express.static(path.join(__dirname, './frontend/build')));
   
-// const express = require('express')
-// const http = require('http')
-// const app = express()
-// const server = http.createServer(app)
-
-// const io = require('socket.io')(server, {
-//     cors: {
-//         origin: "http://localhost:3001",
-//         methods: ["GET", "POST"]
-//     }
-// })
-
-
-// io.on("connection", (socket) => {
-//     socket.emit( "me", socket.id )  //getting own id, to use for connection
-
-//     socket.on( "disconnect", () => { //when call gets disconnected
-//         socket.broadcast.emit( "callEnded" )
-//     })
-
-//     socket.on( "callUser", data => {
-//         io.to(data.userToCall).emit('callUser', {
-//             signal: data.signalData,
-//             from: data.from,
-//             name: data.name
-//         })
-//     })
-
-//     socket.on( "answerCall", data => io.to(data.to).emit( "callAccepted"), data.signalData )
-
-// })
-
-// server.listen(3000, () => console.log('Server is listening on port 3000'))
+    app.get('/*', (req, res) => {
+      res.sendFile(path.join(__dirname, './frontend/build', 'index.html'));
+    });
+  }
